0f7e28d444c8c56ea02828631c8eefed5b59da5a
masterUNG <phrombutr@gmail.com>
2015-11-12 17:49:53 +0700

    Update UI 1

--- app/src/main/java/appewtc/masterung/drugandhealth/MyOpenHelper.java
+++ app/src/main/java/appewtc/masterung/drugandhealth/MyOpenHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 /**
  * Created by masterUNG on 11/10/15 AD.
  */
-public class MyOpenHelper extends SQLiteOpenHelper{
+public class MyOpenHelper extends SQLiteOpenHelper {
 
     //Explicit
     private static final String DATABASE_NAME = "drug.db";
@@ -23,7 +23,31 @@ public class MyOpenHelper extends SQLiteOpenHelper{
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL(CREATE_DRUG_TABLE);
         sqLiteDatabase.execSQL(CREATE_USER_TABLE);
-    }
+
+        //Update drugTABLE
+        String[] strDrug = new String[10];
+        strDrug[0] = "insert into drugTABLE VALUES (null, 'nameDrug0', 'na', 'na', 'คุณสมบัติ', 'วิธีใช้');";
+        strDrug[1] = "insert into drugTABLE VALUES (null, 'nameDrug1', 'na', 'na', 'คุณสมบัติ', 'วิธีใช้');";
+        strDrug[2] = "insert into drugTABLE VALUES (null, 'nameDrug2', 'na', 'na', 'คุณสมบัติ', 'วิธีใช้');";
+        strDrug[3] = "insert into drugTABLE VALUES (null, 'nameDrug3', 'na', 'na', 'คุณสมบัติ', 'วิธีใช้');";
+        strDrug[4] = "insert into drugTABLE VALUES (null, 'nameDrug4', 'na', 'na', 'คุณสมบัติ', 'วิธีใช้');";
+        strDrug[5] = "insert into drugTABLE VALUES (null, 'nameDrug5', 'na', 'na', 'คุณสมบัติ', 'วิธีใช้');";
+        strDrug[6] = "insert into drugTABLE VALUES (null, 'nameDrug6', 'na', 'na', 'คุณสมบัติ', 'วิธีใช้');";
+        strDrug[7] = "insert into drugTABLE VALUES (null, 'nameDrug7', 'na', 'na', 'คุณสมบัติ', 'วิธีใช้');";
+        strDrug[8] = "insert into drugTABLE VALUES (null, 'nameDrug8', 'na', 'na', 'คุณสมบัติ', 'วิธีใช้');";
+        strDrug[9] = "insert into drugTABLE VALUES (null, 'nameDrug9', 'na', 'na', 'คุณสมบัติ', 'วิธีใช้');";
+
+        int intIndex = 0;
+        while (intIndex < strDrug.length) {
+
+            sqLiteDatabase.execSQL(strDrug[intIndex]);
+
+            intIndex += 1;
+        }   // while
+
+
+
+    }   // onCreate
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
