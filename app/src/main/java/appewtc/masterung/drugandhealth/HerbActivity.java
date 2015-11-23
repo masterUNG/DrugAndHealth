0380018a6b63a7b6d0dd44a07ba9c94a853ff343
masterUNG <phrombutr@gmail.com>
2015-11-23 17:11:29 +0700

    Start HerbActivity

---
+++ app/src/main/java/appewtc/masterung/drugandhealth/HerbActivity.java
@@ -0,0 +1,13 @@
+package appewtc.masterung.drugandhealth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HerbActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_herb);
+    }
+}
