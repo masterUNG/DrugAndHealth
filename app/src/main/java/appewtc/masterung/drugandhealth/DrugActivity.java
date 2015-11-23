b7b51e48474a42bce38e11d246829d18ff49d8bf
masterUNG <phrombutr@gmail.com>
2015-11-23 17:59:50 +0700

    Start ListView

--- app/src/main/java/appewtc/masterung/drugandhealth/DrugActivity.java
+++ app/src/main/java/appewtc/masterung/drugandhealth/DrugActivity.java
@@ -2,12 +2,24 @@ package appewtc.masterung.drugandhealth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class DrugActivity extends AppCompatActivity {
+
+    //Explicit
+    private ImageView waterImageView, grainImageView, skinImageView, otherImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drug);
+
+        bindWidget();
+
+    }   // onCreate
+
+    private void bindWidget() {
+
     }
-}
+
+}   // Main Class
