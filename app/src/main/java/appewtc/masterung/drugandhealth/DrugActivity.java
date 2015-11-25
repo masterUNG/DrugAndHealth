943b070c62a979372ad7dbd44fb45b6ea6f31d22
masterUNG <phrombutr@gmail.com>
2015-11-25 16:59:09 +0700

    Intent from Drug to ListView

--- app/src/main/java/appewtc/masterung/drugandhealth/DrugActivity.java
+++ app/src/main/java/appewtc/masterung/drugandhealth/DrugActivity.java
@@ -1,10 +1,12 @@
 package appewtc.masterung.drugandhealth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
-public class DrugActivity extends AppCompatActivity {
+public class DrugActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Explicit
     private ImageView waterImageView, grainImageView, skinImageView, otherImageView;
@@ -16,10 +18,58 @@ public class DrugActivity extends AppCompatActivity {
 
         bindWidget();
 
+        imageController();
+
     }   // onCreate
+
+    private void imageController() {
+        waterImageView.setOnClickListener(this);
+        grainImageView.setOnClickListener(this);
+        skinImageView.setOnClickListener(this);
+        otherImageView.setOnClickListener(this);
+    }
 
     private void bindWidget() {
 
-    }
+        waterImageView = (ImageView) findViewById(R.id.button7);
+        grainImageView = (ImageView) findViewById(R.id.button8);
+        skinImageView = (ImageView) findViewById(R.id.button9);
+        otherImageView = (ImageView) findViewById(R.id.button12);
+
+    }   // bindWidget
+
+    @Override
+    public void onClick(View view) {
+
+        String strDrug = null;
+        int intDrug = 1;
+
+        switch (view.getId()) {
+            case R.id.button7:
+                strDrug = "Drug ชนิดน้ำ";
+                intDrug = 1;
+                break;
+            case R.id.button8:
+                strDrug = "Drug ชนิดเม็ด";
+                intDrug = 2;
+                break;
+            case R.id.button9:
+                strDrug = "Drug ชนิดทา";
+                intDrug = 3;
+                break;
+            case R.id.button12:
+                strDrug = "Drug ชนิดอื่นๆ";
+                intDrug = 4;
+                break;
+        }   // switch
+
+
+        //Intent to ListDrugHerb
+        Intent objIntent = new Intent(DrugActivity.this, ListDrugHerbActivity.class);
+        objIntent.putExtra("Title", strDrug);
+        objIntent.putExtra("key", intDrug);
+        startActivity(objIntent);
+
+    }   // onClick
 
 }   // Main Class
--- app/src/main/java/appewtc/masterung/drugandhealth/ListDrugHerbActivity.java
+++ app/src/main/java/appewtc/masterung/drugandhealth/ListDrugHerbActivity.java
@@ -2,12 +2,41 @@ package appewtc.masterung.drugandhealth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ListDrugHerbActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView titleTextView;
+    private ListView drugListView;
+    private String strTitle;
+    private int keyAnInt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_drug_herb);
+
+        bindWidget();
+
+        showView();
+
+    }   // onCreate
+
+    private void showView() {
+
+        strTitle = getIntent().getStringExtra("Title");
+        keyAnInt = getIntent().getIntExtra("Key", 1);
+
+        //Show Title
+        titleTextView.setText(strTitle);
+
     }
-}
+
+    private void bindWidget() {
+        titleTextView = (TextView) findViewById(R.id.txtShowHead);
+        drugListView = (ListView) findViewById(R.id.listView);
+    }
+
+}   // Main Class
