9f36d5457aefc23d4728a1d84666fa656748975a
masterUNG <phrombutr@gmail.com>
2015-11-25 17:17:57 +0700

    Show View Finish

--- app/src/main/java/appewtc/masterung/drugandhealth/HerbActivity.java
+++ app/src/main/java/appewtc/masterung/drugandhealth/HerbActivity.java
@@ -1,13 +1,58 @@
 package appewtc.masterung.drugandhealth;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
-public class HerbActivity extends AppCompatActivity {
+public class HerbActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Explicit
+    private ImageView herbOutImageView, herbInImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_herb);
+
+        bindWidget();
+
+        imageController();
+
+    }   // onCreate
+
+    private void imageController() {
+        herbOutImageView.setOnClickListener(this);
+        herbInImageView.setOnClickListener(this);
     }
-}
+
+    private void bindWidget() {
+        herbOutImageView = (ImageView) findViewById(R.id.button10);
+        herbInImageView = (ImageView) findViewById(R.id.button11);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        String strTitle = null;
+        int intKey = 0;
+
+        switch (view.getId()) {
+            case R.id.button10:
+                strTitle = "Herb ยาใช้ภายนอก";
+                intKey = 5;
+                break;
+            case R.id.button11:
+                strTitle = "Herb ยาใช้ภายใน";
+                intKey = 6;
+                break;
+        }   // switch
+
+        Intent objIntent = new Intent(HerbActivity.this, ListDrugHerbActivity.class);
+        objIntent.putExtra("Title", strTitle);
+        objIntent.putExtra("Key", intKey);
+        startActivity(objIntent);
+
+    }
+}   // Main Class
