9aaf6722f14c12da6cfd8a0a5c66aede7933ae22
masterUNG <phrombutr@gmail.com>
2015-12-01 18:03:45 +0700

    Finish 1Dec

--- app/src/main/java/appewtc/masterung/drugandhealth/PersonalActivity.java
+++ app/src/main/java/appewtc/masterung/drugandhealth/PersonalActivity.java
@@ -3,14 +3,38 @@ package appewtc.masterung.drugandhealth;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class PersonalActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameUserEditText, historyEditText, usedEditText,
+            allergiesEditText, resistanceEditText, myDrugEditText;
+    private RadioGroup alertRadioGroup;
+    private RadioButton choice1RadioButton, choice2RadioButton, choice3RadioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal);
+
+        //Bind Widget
+        bindWidget();
+
     }   // onCreate
+
+    private void bindWidget() {
+
+        nameUserEditText = (EditText) findViewById(R.id.editText);
+        historyEditText = (EditText) findViewById(R.id.editText3);
+        usedEditText = (EditText) findViewById(R.id.editText4);
+        allergiesEditText = (EditText) findViewById(R.id.editText5);
+        resistanceEditText = (EditText) findViewById(R.id.editText6);
+        myDrugEditText = (EditText) findViewById(R.id.editText8);
+
+    }
 
     public void clickSavePersonal(View view) {
 
